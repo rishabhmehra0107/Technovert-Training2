05b8e21e1f79cf73f54919d7c0ad352287fe260c
Rishabh <rishabhmehra0107@gmail.com>
2021-02-03 15:38:44 +0530

    Commit

---
+++ Aggregation_Has-a-Relationship/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+public class Marks
+{
+    public string subject;
+    public int score;
+
+    public Marks(string subject, int score)
+    {
+        this.subject = subject;
+        this.score = score;
+    }
+}
+public class Student
+{
+    public string name;
+    public int id;
+    public Marks marks;
+
+    public Student(string name, int id, Marks marks)
+    {
+        this.name = name;
+        this.id = id;
+        this.marks = marks;
+    }
+    public void display()
+    {
+        Console.WriteLine(id + " " + name + " " + marks.subject + " " + marks.score);
+    }
+}
+public class TestAggregation
+{
+    public static void Main(string[] args)
+    {
+        Marks m = new Marks("Maths", 98);
+        Student s = new Student("Rishabh", 1, m);
+        s.display();
+    }
+}
