8b335dabadebd3f890cb1f74a2eb5d8f8fa463e3
Rishabh <rishabhmehra0107@gmail.com>
2021-02-04 02:17:19 +0530

    Commit

---
+++ Generics1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Generics
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool result = Calculator.Equal<int>(1, 2);
+            if (result == true)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+        }
+    }
+    public class Calculator
+    {
+        public static bool Equal<T>(T val1, T val2)
+        {
+            return val1.Equals(val2);
+        }
+    }
+}
---
+++ Generics_Function_Overloading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Generics_Function_Overloading
+{
+    public class Example
+    {
+        public void Animal1<T>(T val)
+        {
+            Console.WriteLine(val.GetType().Name);
+        }
+        public void Animal1<T>(int x)
+        {
+            Console.WriteLine(x.GetType().Name);
+        }
+        public void Animal2<T>(T x)
+        {
+            Animal1(x);
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Example ex = new Example();
+            ex.Animal1("Testing data");
+            ex.Animal1(90);
+            ex.Animal2(5050);
+        }
+    }
+}
