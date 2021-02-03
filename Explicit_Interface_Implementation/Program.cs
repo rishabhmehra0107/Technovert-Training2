80935adea7ea3fa8463d8a1b505ed6d3c09f00f7
Rishabh <rishabhmehra0107@gmail.com>
2021-02-03 15:24:31 +0530

    c

---
+++ Explicit_Interface_Implementation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+interface ICustomer
+{
+    void Print();
+}
+
+interface ICustomer2
+{
+    void Print();
+}
+
+public class Program : ICustomer, ICustomer2
+{
+    void ICustomer.Print()
+    {
+        Console.WriteLine("Interface Example");
+    }
+    void ICustomer2.Print()
+    {
+        Console.WriteLine("Hello Rishabh");
+    }
+    public static void Main()
+    {
+        Program P = new Program();
+        ((ICustomer)P).Print();
+        ((ICustomer2)P).Print();
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+interface ICustomer
+{
+    void Print();
+}
+
+interface ICustomer2
+{
+    void Print2();
+}
+class Customer : ICustomer, ICustomer2
+{
+    public void Print()
+    {
+        Console.WriteLine("Interface Example");
+    }
+    public void Print2()
+    {
+        Console.WriteLine("Hello Rishabh");
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Customer C1 = new Customer();
+        C1.Print();
+        C1.Print2();
+    }
+}
