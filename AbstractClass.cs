d475a3f2df53dbfa94a0b6ebac774a9aed4b4e04
rishabhmehra0107 <68009945+rishabhmehra0107@users.noreply.github.com>
2021-02-03 15:46:14 +0530

    Add files via upload

---
+++ AbstractClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public abstract class Customer
+{
+    public abstract void Print();
+    public void Print2()
+    {
+        Console.WriteLine("Technovert");
+    }
+}
+
+
+
+public class Program : Customer
+{
+    public override void Print()
+    {
+        Console.WriteLine("Abstract class Example");
+    }
+
+    public static void Main()
+    {
+        Customer C = new Program();
+        C.Print();
+        C.Print2();
+    }
+}
