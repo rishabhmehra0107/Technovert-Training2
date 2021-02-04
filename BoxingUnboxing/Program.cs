31a5c78db3636492e42dc0f8542aea963b755503
Rishabh <rishabhmehra0107@gmail.com>
2021-02-04 16:51:28 +0530

    commit

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BoxingUnboxing
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int i = 200;
+            object o = i;
+            try
+            {
+                int j = (short)o;
+                System.Console.WriteLine("Unboxing works fine");
+            }
+            catch(System.InvalidCastException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
